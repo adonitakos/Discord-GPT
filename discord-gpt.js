5853c2acdbc5a0223dc1be737d9b64ada2c50f05
adonitakos <antonios.takos19@stjohns.edu>
2023-06-06 14:46:05 -0400

    Re-commit and push of Discord-GPT bot

--- discord-gpt.js
+++ discord-gpt.js
@@ -1,45 +1,39 @@
 // Import environment variables
-require('dotenv').config()
+require('dotenv').config();
 
 // ========= OpenAI Functionality ========= \\
+
 const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-}); // <--- OpenAI configuration ends here
+
+const OPENAIAPI_KEY = `${process.env.OPENAIAPI_KEY}`;
+const ORG_KEY = `${process.env.ORG_KEY}`;
+
+const configuration  = new Configuration ({
+    organization: ORG_KEY,
+    apiKey: OPENAIAPI_KEY,
+});
 
 const openai = new OpenAIApi(configuration);
 
-const gptQuery = async(queryPrompt) => {
-    const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: queryPrompt }],
-    }).then(response => {
-        let res = response["data"]["choices"][0]["message"]["content"];
-        return res
-    }); // <--- .then(response) ends here
-    return response;
-};  // <--- gptQuery() async function ends here
+const gptQuery = async(message) => {
 
-// async function gptQuery(query) { 
-//   const openai = new OpenAIApi(configuration);
-//   const response = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: query,
-//     max_tokens: 2048,
-//     temperature: 1.0,
-//   }).then(response => {
-//       let res = response.data.choices[0].text
-//       return res; // <-- return the value of res
-//   }) // <--- .then(response) ends here
-//   return response
-// } // <--- gptQuery() function ends here
+    const completion = await openai.createChatCompletion({
+        model: "gpt-4",
+        messages: [
+            {role: "user", content: `${message}`},
+        ]
+    }); // <--- completion await function ends here
+
+    return completion.data.choices[0].message;
+
+} // <--- gptQuery() async function ends here
 
 // =============== Discord Bot Functionality =============== \\
 
-const { Client, Events, GatewayIntentBits, SlashCommandBuilder, SlashCommandStringOption } = require('discord.js');
+const { Client, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
-client.once(Events.ClientReady, client => {
+client.on(Events.ClientReady, client => {
   console.log(`Logged in as ${client.user.tag}!`);
   
   const ping = new SlashCommandBuilder()
@@ -56,7 +50,7 @@ client.once(Events.ClientReady, client => {
 
   const gpt_query = new SlashCommandBuilder()
     .setName('gpt_query')
-    .setDescription('Query from GPT-3.5 model')
+    .setDescription('Query from GPT model')
     .addStringOption(option => 
       option.setName('query')
         .setDescription('What you will input to be queried'));
@@ -102,7 +96,9 @@ client.on(Events.InteractionCreate, async interaction => {
 
     if(interaction.commandName === 'gpt_query') {
       const result = await gptQuery(interaction.options.getString('query'));
+      console.log("========================");
       console.log(result);
+      console.log("========================");
       interaction.reply(result);
     }
   
@@ -116,10 +112,9 @@ client.login(process.env.DISCORD_KEY);
 const keepAlive = require('./server');
 keepAlive();
 
+// ===== Commands that use $ ===== \\
 
-
-// ===== Code that might need to be re-used later ===== \\
-
+// $ gptQuery command
 client.on('messageCreate', async message => {
   if(message.content.startsWith('$')) {
     let userQuery = message.content.slice(1)
@@ -131,73 +126,8 @@ client.on('messageCreate', async message => {
 
 
 // $ ping command
-// client.on('messageCreate', message => {
-//     if (message.content == '$ping') {
-//       message.reply('Pong!');
-//     }
-// }); // <--- ping-pong function ends here
-
-
-// const clientId = '1050671337049423932'
-
-// const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_KEY);
-
-// (async () => {
-//   try {
-//     console.log('Started refreshing application (/) commands.');
-
-//     await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-//     console.log('Successfully reloaded application (/) commands.');
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
-
-
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}!`);
-// }); // <--- client ready function ends here
-
-// client.on('interactionCreate', async interaction => {
-//   if (!interaction.isChatInputCommand()) return;
-
-//   if (interaction.commandName === 'ping') {
-//     await interaction.reply('Pong!');
-//   }
-
-//   if (interaction.commandName === 'add') {
-//     const num1 = interaction.options.get('first-number').value;
-//     const num2 = interaction.options.get('second-number').value;
-//     console.log(`The numbers received were: ${num1} and ${num2}`);
-//     // interaction.reply(`The sum is ${num1 + num2}`);
-    
-//   }
-
-//   if (interaction.commandName === 'gpt_query') {
-//     let userQuery = interaction.options.get('query');
-//     const result = await gptQuery(userQuery);
-//     await interaction.reply(result);
-//   }
-  
-// }); // <--- interactionCreate ends here
-
-
-// ==== Needed when DELETING all the application commands ==== \\
-
-// const { Client, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
-// const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
-
-
-// const { REST, Routes } = require('discord.js');
-// const clientId = '1050671337049423932'
-// const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_KEY);
-
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}!`);
-// }); // <--- client ready function ends here
-
-// // for global commands
-// rest.put(Routes.applicationCommands(clientId), { body: [] })
-// 	.then(() => console.log('Successfully deleted all application commands.'))
-// 	.catch(console.error);
+client.on('messageCreate', message => {
+    if (message.content == '!ping') {
+      message.reply('Pong!');
+    }
+}); // <--- ping-pong function ends here
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
+app.get('/', (res) =>{
     res.send("<br><h1>If you are on this page, it means that the Discopen AI bot should be running!</h1>")
 });
 
-
 function keepAlive() {
     app.listen('3000', () => {
-        console.log(`\nServer is running on port 3000 | ${Date.now()}`);
+        console.log("\nServer is running on port 3000");
     });
 } // <--- keepAlive() function ends here
 
